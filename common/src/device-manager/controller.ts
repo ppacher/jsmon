d6a767bfd9923efa91b6d30ef9b5c667c593bb8e
Patrick Pacher <p.pacher@cybertrap.com>
2018-01-30 20:01:37 +0100

    common/device-manager: added support for device commands anda param validation

--- common/src/device-manager/controller.ts
+++ common/src/device-manager/controller.ts
@@ -1,3 +1,6 @@
+import {Observable} from 'rxjs/Observable';
+import {_throw} from 'rxjs/observable/throw';
+import {fromPromise} from 'rxjs/observable/fromPromise';
 
 export namespace Device {
     /** Available types for command paramters */
@@ -30,7 +33,7 @@ export namespace Device {
         };
         
         /** The handle function to invoke for the command */
-        handler: (...args: any[]) => any;
+        handler: (params: Map<string, any>) => Promise<any>;
     } 
     
     /**
@@ -51,5 +54,92 @@ export namespace Device {
             /** An optional description of the device */
             public description: string = ''
         ) {}
+        
+        /**
+         * Executes a command with the provided arguments and returns
+         * and observable that emits the results when available
+         *
+         * @param command   The name of the command to execute
+         * @param args      Additional arguments to pass to the command
+         */
+        call(command: string, params: Map<string, any>): Observable<any> {
+            const cmd = this.commands.find(c => c.name == command);
+
+            if (cmd === undefined) {
+                return _throw(new Error(`Command ${command} not supported by device ${this.name}`));
+            }
+            
+            let errors = Device.validateParameters(cmd, params);
+            
+            if (errors !== null) {
+                throw new Error(errors.map(err => err.message).join(', '));
+            }
+            
+            return fromPromise(cmd.handler(params));
+        }
+        
+        /**
+         * Validates a parameter map against a schema definition
+         * 
+         * @param schema  The {@link CommandSchema} definition
+         * @param args    The map containing the parameters for the call
+         *
+         * @returns A list of errors or null
+         */
+        static validateParameters(schema: CommandSchema, args: Map<string, any>): Error[]|null {
+            let errors: Error[] = [];
+            
+            let iterator = args.entries();
+
+            let item: IteratorResult<any>;
+            
+            // unfortunately typescript does not yet support
+            // for .. of for iterators :/
+            item = iterator.next();
+            while(!item.done) {
+                const key = item.value[0];
+                const value = item.value[1];
+                
+                // Get the next value ASAP
+                item = iterator.next();
+                
+                const definition = schema.parameters[key];
+
+                if (definition == undefined) {
+                    errors.push(new Error(`Unkown parameter ${key} for command ${schema.name}`));
+                    continue;
+                }
+                
+                const types: ParameterType[] = definition instanceof Array ? definition : definition.types;
+                
+                if (types.includes(ParameterType.Any)) {
+                    // Every type of parameter is allowed, skip checks
+                    continue;
+                }
+                
+                const isValidType = types.some(ptype => typeof value === ptype);
+                
+                if (!isValidType) {
+                    errors.push(new Error(`Invalid type for parameter ${key} on command ${schema.name}`));
+                    continue;
+                }
+            }
+            
+            // we also need to check if there are missing required parameters
+            Object.keys(schema.parameters).forEach(parameterName => {
+                const definition = schema.parameters[parameterName];
+                const optional = Array.isArray(definition) ? false : definition.optional;
+
+                if (!optional && args[parameterName] === undefined) {
+                    errors.push(new Error(`Missing required parameter ${parameterName} form command ${schema.name}`));
+                }
+            });
+            
+            if (errors.length == 0) {
+                return null;
+            }
+            
+            return errors;
+        }
     };
 }
