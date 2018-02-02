07ee603f990f99335908de5bac284b95af1a596f
Patrick Pacher <p.pacher@cybertrap.com>
2018-02-02 18:23:13 +0100

    Added some comments to the example

--- example/src/index.ts
+++ example/src/index.ts
@@ -33,8 +33,13 @@ export class App {
                 private _notify: NotificationService) {
         this._server.listen(9080);
         
+        // Create a new device for MPD that connects to 127.0.0.1:6600 (defaults of MPDConfig.new())
+        // This will expose any sensors and commands under http://localhost:9080/devices/mpd:localhost/
         let controller = this._device.setupDevice('mpd:localhost', MPDDevice, 'description', MPDConfig.new());
         
+        // Setup a listener for 'title', 'artist' and 'ablum' state changes
+        // on the MPD device to send desktop notifications about the current
+        // track
         controller.watchSensor('title')
                     .combineLatest(
                         controller.watchSensor('artist'),
