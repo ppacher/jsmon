9429395181e004f14766873a8dd27c6a0a0153de
Patrick Pacher <p.pacher@cybertrap.com>
2018-03-01 09:22:14 +0100

    Updated build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,43 @@
 #!/bin/bash
 
-for pkg in core common example ; do
-    echo "Building $pkg"
-    
-    pushd $pkg 2>/dev/null
-    npm update && npm install && npm run build
-    popd 2>/dev/null
-done
+npm install rxjs
+
+base=$(pwd)
+
+buildPackage() {
+    cd ${base}/packages/$1
+
+    npm start
+
+    cd ${base}
+}
+
+buildApp() {
+    cd ${base}/$1
+
+    shift
+
+    npm install
+    npm start -- $@
+
+    cd ${base}
+}
+
+skipLibs=false
+
+if [ "$1" == "--skip-libs" ]; then
+    skipLibs=true
+fi
+
+if [ $skipLibs == false ]; then
+    buildPackage core
+    buildPackage plugin-httpserver
+    buildPackage plugin-mpd
+    buildPackage plugin-mqtt
+    buildPackage plugin-sysinfo
+    buildPackage plugin-graphql
+    buildPackage plugin-darkskynet
+fi
+
+
+buildApp example --config config.json
