331b2d2201900de7b5dd0bf6f2656a35eec4da71
Patrick Pacher <p.pacher@cybertrap.com>
2018-06-12 18:49:16 +0200

    platform-sysinfo: Added OnDestroy support

--- platforms/platform-sysinfo/src/sysinfo.device.ts
+++ platforms/platform-sysinfo/src/sysinfo.device.ts
@@ -1,43 +1,55 @@
+import {OnDestroy} from '@homebot/core';
 import {Device, Sensor, ParameterType} from '@homebot/platform';
 import * as sysinfo from 'systeminformation';
 import {Observable} from 'rxjs/Observable';
 import {BehaviorSubject} from 'rxjs/BehaviorSubject';
+import {Subject} from 'rxjs/Subject';
 import {fromPromise} from 'rxjs/observable/fromPromise';
 import {interval} from 'rxjs/observable/interval';
 import {of} from 'rxjs/observable/of';
-import 'rxjs/add/operator/mergeMap';
-import 'rxjs/add/operator/do';
-import 'rxjs/add/operator/catch';
-import 'rxjs/add/operator/filter';
-import 'rxjs/add/operator/share';
+
+import {flatMap, share, catchError, filter, takeUntil} from 'rxjs/operators';
 
 import {Systeminformation} from 'systeminformation';
 
-function poll<T>(p: () => Promise<T>): Observable<T> {
+function poll<T>(until: Observable<any>, p: () => Promise<T>): Observable<T> {
     return interval(10000)
-            .flatMap(
-                () => fromPromise(p())
-                        .catch(err => of(new Error(err)))
-                        .filter(s => !(s instanceof Error)) as Observable<T>
-            ).share();
+            .pipe(
+                takeUntil(until),
+                flatMap(
+                    () => fromPromise(p())
+                            .pipe(
+                                catchError(err => of(new Error(err))),
+                                filter(s => !(s instanceof Error)),
+                            )
+                ),
+                share()
+            ) as any as Observable<T>;
 }
 
 @Device({
     description: 'System information',
 })
 export class SysInfoDevice {
+    private readonly _destroyed: Subject<void> = new Subject();
+    
     @Sensor({name: 'memory', type: ParameterType.OBJECT})
-    readonly memory = poll<Systeminformation.MemData>(sysinfo.mem);
+    readonly memory = poll<Systeminformation.MemData>(this._destroyed, sysinfo.mem);
     
     @Sensor({name: 'cpu', type: ParameterType.OBJECT})
-    readonly cpu = poll<Systeminformation.CpuData>(sysinfo.cpu);
+    readonly cpu = poll<Systeminformation.CpuData>(this._destroyed, sysinfo.cpu);
     
     @Sensor({name: 'battery', type: ParameterType.OBJECT})
-    readonly battery = poll<Systeminformation.BatteryData>(sysinfo.battery);
+    readonly battery = poll<Systeminformation.BatteryData>(this._destroyed, sysinfo.battery);
     
     @Sensor({name: 'load', type: ParameterType.OBJECT})
-    readonly load = poll<Systeminformation.CurrentLoadData>(sysinfo.currentLoad);
+    readonly load = poll<Systeminformation.CurrentLoadData>(this._destroyed, sysinfo.currentLoad);
     
     @Sensor({name: 'fs', type: ParameterType.OBJECT})
-    readonly fs = poll<Systeminformation.FsSizeData[]>(sysinfo.fsSize);
+    readonly fs = poll<Systeminformation.FsSizeData[]>(this._destroyed, sysinfo.fsSize);
+    
+    onDestroy() {
+        this._destroyed.next();
+        this._destroyed.complete();
+    }
 }
